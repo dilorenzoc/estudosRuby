5b547946c5aa2315922995aec94b10b721665be8
Camila Di Lorenzo <43864961+dilorenzoc@users.noreply.github.com>
2019-04-24 17:15:21 -0300

    Add files via upload

---
+++ calculadoraB.rb
@@ -0,0 +1,9 @@
+print 'digite o primeiro numero: '
+n1 = gets.chomp.to_i
+print 'digite o segundo numero: '
+n2 = gets.chomp.to_i
+
+puts"Resultado da soma: #{n1+n2} \n
+Resultado da subtração: #{n1-n2}\n
+Resultado da divisão: #{n1/n2}\n
+Resultado da multiplicação: #{n1*n2}"
